60a9cb5e3bafa3f1e0cbadf850a728431a173bce
PhamLiem1124 <113281005+PhamLiem1124@users.noreply.github.com>
2022-11-30 15:05:31 +0700

    C3: connect firebase

--- app/src/main/java/com/example/musicandroid/MainActivity.java
+++ app/src/main/java/com/example/musicandroid/MainActivity.java
@@ -4,11 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DatabaseReference database = FirebaseDatabase.getInstance("https://musicandroidjava-default-rtdb.asia-southeast1.firebasedatabase.app/").getReference();
+        database.child("test").setValue("test 001");
+
     }
 }
